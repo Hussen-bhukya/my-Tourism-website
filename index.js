2094873ed89de6505ec34b48602a25a52a9f40f1
Hussen Bhukya <hussenbhukya702@gmail.com>
2025-07-11 14:14:00 +0530

    commiting new changes

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ let a = "hello world";
 let aa = "welcome";
 let ac = a +' '+aa;
 console.log(ac);
+
+let First_var = 40;
+let Second_Var = 50;
+let Total = First_var - Second_Var;
+
+console.log(Total);
